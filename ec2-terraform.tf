fab5f85966633d583f59504b8a70335054353203
sateesh sabbineni <sateesh.sabbineni@outlook.com>
2021-05-29 15:51:24 -0400

    ignore file added

--- ec2-terraform.tf
+++ ec2-terraform.tf
@@ -1,4 +1,5 @@
-resource "aws-instance" "tf-ec2" {
+resource "aws_instance" "tf-ec2" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
 }
+
--- main.tf
+++ main.tf
@@ -1,5 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+
+}
+
+}
+
+# Configure the AWS Provider
 provider "aws" {
+  alias  = "east"
+  profile = "default"
+  region  = "us-east-2"
   access_key = "AKIA5HJZIVL47SSGZKES"
   secret_key = "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"
-  region = "us-east-2"
 }
+
+# resource "aws-instance" "tf-ec2" {
+#   ami = "ami-077e31c4939f6a2f3"
+#   instance_type = "t2.micro"
+# }
